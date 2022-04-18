6e06bf15d71a63149227186e88469f66758221cb
Umut <71285287+umt01@users.noreply.github.com>
2022-04-18 20:48:49 +0300

    Update Ready.js

--- Events/Ready.js
+++ Events/Ready.js
@@ -1,7 +1,7 @@
 const client = global.client;
 module.exports = () => {
   console.log("Bot başarıyla aktif oldu!");
-  client.user.setActivity("BotClub ❤ umt`#1000"); //Botunuzun Durum Ksımında Yazıcak Yazı.
+  client.user.setActivity("umt#0001 Tarafından Kodlanmıştır. - discord.gg/dbc"); //Botunuzun Durum Ksımında Yazıcak Yazı.
 }
 module.exports.configuration = {
   name: "ready"
